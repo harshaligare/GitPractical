28a19a8031f24965d130dedb4a0b673af28589a8
Group011 <Group011@infoway.com>
2023-02-02 09:59:41 +0530

    Add java demo application files

---
+++ src/git/DemoJava.java
@@ -0,0 +1,18 @@
+package git;
+
+public class DemoJava {
+
+	public static void main(String[] args) {
+		
+			System.out.println(checkIfInputIsAnEvenNumber(122));
+			
+			}
+
+			public static boolean checkIfInputIsAnEvenNumber(int
+
+			number){
+			return number % 2 == 0;
+
+	}
+
+}
